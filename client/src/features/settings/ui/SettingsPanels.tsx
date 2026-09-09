a7c5fb6a9fe9df4f79e309c30cb3ab5449e09de6
Eviatar <eviatarmor8@gmail.com>
2026-09-09 18:05:53 +1000

    feat(settings): add CorePanels and SettingsPanels barrel

---
+++ client/src/features/settings/ui/SettingsPanels.tsx
@@ -0,0 +1,11 @@
+export {
+  AppearancePanel,
+  NotificationsPanel,
+  ProfilePanel,
+} from "./CorePanels"
+export { BillingPanel, PrivacyPanel } from "./BillingPrivacyPanels"
+export {
+  CompliancePanel,
+  SecurityPanel,
+  WorkspacePanel,
+} from "./SecurityCompliancePanels"
